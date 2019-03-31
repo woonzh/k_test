23af02e7926a64f13fed7d009a33d80a2bbe0b0f
woonzh <zhenhao1991@gmail.com>
2019-03-31 15:00:54 +0800

    first

---
+++ test.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from datetime import datetime
+from dateutil import parser
+
+def convertStringToTime(string):
+    obj=datetime.strptime(string, '%d/%m/%Y')
+    return obj
+
+def productStringSplit(string):
+    products=['Product_S' ,'Product_B','Product_C']
+    store=string.split('  ')
+    
+    results=[1 if x in store else 0 for x in products]
+    return results
+
+def productBreakdown(stringLst):
+    temDf=pd.DataFrame(columns=['Product_S' ,'Product_B','Product_C'])
+    for count, val in enumerate(list(stringLst)):
+        temDf.loc[count]=productStringSplit(val)
+        
+    return temDf
+
+def dataClean(df, fname):
+    df['Date of Visit']=[convertStringToTime(x) for x in df['Date of Visit']]
+    productDf=productBreakdown(df['Product_List'])
+    df=pd.concat([df, productDf], axis=1)
+    
+    df.to_csv(fname, index=False)
+    
+    return df
+
+fname='Customer Visits Interview Exercise Data.csv'
+cleanFname='Customer Visits Interview Exercise Data (cleaned).csv'
+
+df=pd.read_csv(fname)
+cleanedDf=dataClean(df,cleanFname)
+
+cleanedDf=pd.read_csv(cleanFname)
